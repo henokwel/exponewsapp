035a78d35c0bbf80c775bc5bc8538a5d2f973cff
henokwel <henokwel@outlook.com>
2022-01-19 01:40:15 +0100

    native base added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider ,Box} from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#000" alignItems="center" justifyContent="center">
+        Hello world</Box>
+    </NativeBaseProvider>
   );
 }
 
