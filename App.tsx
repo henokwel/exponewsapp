275b02ba595620c49c68fe230ad8091e304171c5
henokwel <henokwel@outlook.com>
2022-01-19 21:51:44 +0100

    init tab nav added

--- App.tsx
+++ App.tsx
@@ -1,16 +1,42 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider, Box } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, Box, Text } from "native-base";
+// import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/Header";
 
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
 export default function App() {
+  const Tab = createMaterialTopTabNavigator();
+
   return (
-    <NativeBaseProvider>
-      <Header />
-      <Box flex={1} bg="#000" alignItems="center" justifyContent="center">
-        Hello world
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Header />
+
+        <Box flexDirection="row">
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </Box>
+        {/* <Box  bg="#000" alignItems="center" justifyContent="center">
+          Hello world
+        </Box> */}
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
- 
+
+function HomeScreen() {
+  return (
+    <Box bg="#000" alignItems="center" justifyContent="center">
+     <Text> Home
+       </Text> 
+    </Box>
+  );
+}
+
+function SettingsScreen() {
+  return <Text>SettingsScreen</Text>;
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Box, Container, Heading, Text } from "native-base";
+import { Box, Container, Flex, Heading, Text, View } from "native-base";
 // import { StatusBar } from "expo-status-bar";
-import { View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+
+const Tab = createMaterialTopTabNavigator();
 
 const Header: React.FC<{}> = () => {
   return (
@@ -10,23 +14,50 @@ const Header: React.FC<{}> = () => {
 
       <View style={{ paddingTop: StatusBar.currentHeight }} />
 
-      <Box>
+      <Box
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        width="96"
+        
+      >
         <Heading color="black" fontWeight="bold" letterSpacing="md">
           {" "}
           Reading
         </Heading>
 
-        <Box>
-            <Text>Search</Text>
-            <Text>Avatar</Text>
-        </Box>
+        <Flex direction="row">
+          <Text>Search</Text>
+          <Text>Avatar</Text>
+        </Flex>
       </Box>
 
-      <Box>
-        <Text>Tabs</Text>
-      </Box>
+      {/* <Box flexDirection="row">
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </Box> */}
     </Container>
   );
 };
+
+
+function HomeScreen() {
+    
+    return(
+        <Box flex={1} bg="#000" alignItems="center" justifyContent="center">
+          Home
+        </Box>
+    )
+}
+
+function SettingsScreen() {
+    
+    return(
+        <Text>SettingsScreen</Text>
+    )
+}
+
 
 export default Header;
